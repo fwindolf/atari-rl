6e9f71cd670e0096235fa78879fbe556a079a49b
ga87six <florian.windolf@tum.de>
2018-01-06 19:50:53 +0100

    Add cuda check to model

--- models/dqn.py
+++ models/dqn.py
@@ -9,12 +9,17 @@ class DQN(ModelBase):
     """
     DQN convolutional network that produces Q-values
     
-    num_inputs (int)  : The length of the input-frame array
+    num_inputs (int)  : The number of channels of the input frame array
     num_actions (int) : The number of actions in action_space 
     """
     def __init__(self, num_inputs, num_actions):
         super().__init__(num_inputs, num_actions)
         
+        if self.is_cuda():
+            self.cuda()
+        
+        # TODO: This only works for certain input resolutions! (80,80)
+
         # Network definition
         self.conv1 = nn.Conv2d(self.inputs, 16, kernel_size=5, stride=2)
         self.bn1 = nn.BatchNorm2d(16)
