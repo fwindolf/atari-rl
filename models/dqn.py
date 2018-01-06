feef17026a546045aae2d4810f4af7ea9c7efc58
ga87six <florian.windolf@tum.de>
2018-01-06 19:51:18 +0100

    Make model work for (80,80) resolution

--- models/dqn.py
+++ models/dqn.py
@@ -21,17 +21,19 @@ class DQN(ModelBase):
         # TODO: This only works for certain input resolutions! (80,80)
 
         # Network definition
-        self.conv1 = nn.Conv2d(self.inputs, 16, kernel_size=5, stride=2)
+        self.conv1 = nn.Conv2d(num_inputs, 16, kernel_size=5, stride=2)
         self.bn1 = nn.BatchNorm2d(16)
         self.conv2 = nn.Conv2d(16, 32, kernel_size=5, stride=2)
         self.bn2 = nn.BatchNorm2d(32)
         self.conv3 = nn.Conv2d(32, 32, kernel_size=5, stride=2)
-        self.bn3 = nn.BatchNorm2d(32)
-        self.head = nn.Linear(448, self.actions)
+        self.bn3 = nn.BatchNorm2d(32)        
+        self.fc4 = nn.Linear(7 * 7 * 32, 512)
+        self.fc5 = nn.Linear(512, num_actions)
         
     def forward(self, x):
         x = F.relu(self.bn1(self.conv1(x)))
         x = F.relu(self.bn2(self.conv2(x)))
         x = F.relu(self.bn3(self.conv3(x)))
-        return self.head(x.view(x.size(0), -1))
+        x = F.relu(self.fc4(x.view(x.size(0), -1)))
+        return self.fc5(x)
     
