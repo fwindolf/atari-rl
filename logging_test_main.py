ad1b75e65efb4d7d3809f5dee7f2a4b8bda4d0b8
tom <tom@tom.localdomain>
2018-01-13 12:24:00 +0100

    implemented overfitting test into logging_test_main, extended data_loader.py to select special samples for overfitting

--- logging_test_main.py
+++ logging_test_main.py
@@ -25,16 +25,11 @@ plt.imshow(frame);
 
 data_dir = '/mnt/c/Users/sc_to/Documents/IN2346_Deep_Leaning_Exercise/Final_Project/atari_data'
 dataset = AtariGrandChallengeDataset(data_dir, 'spaceinvaders', history_len=10, screen=screen, max_files=2)
-data_train, data_valid, data_test = dataset.split(0.7, 0.2)
+data_train, data_valid, data_test = dataset.split(0.7, 0.2,'OVERFIT')
+print('Size of Training Data: %d' % len(data_train))
 
 agent = DQNAgent(screen, mem_size=1000)
 agent.initialize(800)
-
-transition = data_train.raw(146) # get as Transition
-observation = screen.output(transition.observation)
-plt.imshow(observation)
-print("Action:", screen.get_action_meaning(transition.action))
-print("Predicted:", screen.get_action_meaning(agent.next_action(observation)))
 
 batchsize = 1
 
@@ -45,6 +40,6 @@ val_loader = DataLoader(data_valid, batch_size=batchsize, num_workers=4)
 logfile_path = '/mnt/c/Users/sc_to/Documents/IN2346_Deep_Leaning_Exercise/Final_Project/dl4cv/logfile.log'
 console_log_level = 'DEBUG'
 solver = Solver(optim.Adam, CrossEntropyLoss(), batchsize, logfile_path = logfile_path, log_level=console_log_level)
-solver.train_offline(agent, train_loader, val_loader, num_epochs=10, log_nth=0)
+solver.train_offline(agent, train_loader, val_loader, num_epochs=50, log_nth=0)
 #solver.train_online(agent, screen)
 #solver.play(agent, screen)
