da331c24d1f7c7941fad951d2a3d429e90e6f51b
ga87six <florian.windolf@tum.de>
2018-01-25 14:27:10 +0100

    Improved traning

--- dqn_spaceinvaders_train_dataset.py
+++ dqn_spaceinvaders_train_dataset.py
@@ -1,7 +1,7 @@
 import logging
 import torch
 from torch import optim
-from torch.nn import CrossEntropyLoss
+from torch.nn import CrossEntropyLoss, SmoothL1Loss
 from torch.utils.data import DataLoader
 
 from torchvision import transforms, utils
@@ -13,8 +13,6 @@ from train.solver import Solver
 
 HISTORY_LEN = 4
 BATCHSIZE = 32
-MEMORY_SIZE = 1000000
-INITIAL_SIZE = 500
 INITIAL_LEN = 1
 EPOCHS = 1000
 DATA_PATH = '/mnt/e/data/atari_v2_release'
@@ -31,20 +29,26 @@ logger.info("Opening dataset")
 dataset = AGCDataSet(DATA_PATH, GAME, history_len=HISTORY_LEN, screen=screen)
 
 logger.info("Creating Agent")
-agent = DQNAgent(screen, history_len=HISTORY_LEN, mem_size=MEMORY_SIZE)
+agent = DQNAgent(screen, history_len=HISTORY_LEN, mem_size=0, loss=SmoothL1Loss())
+agent.model.save('/output/dqn_base_spaceinvaders.pt')
+
 logger.info("GPU available: %r" % torch.cuda.is_available())
 logger.info("Model on GPU: %r" % agent.model.is_cuda)
 
 data_loader = DataLoader(dataset, batch_size=BATCHSIZE, shuffle=True, num_workers=4)
-solver = Solver(optim.Adam, CrossEntropyLoss(), batchsize=BATCHSIZE, log_level='INFO')
+solver = Solver(optim.RMSprop, None, batchsize=BATCHSIZE, log_level='INFO') # Loss doesnt do anything for dataset learning
 
 logger.info("Creating Baseline scores")
 best_score, mean_score, mean_dur = solver.play(agent, screen, num_sequences=INITIAL_LEN)
 logger.info("Baseline with best score %d (Mean %d in %d frames)" % (best_score, mean_score, mean_dur))
 
 logger.info("Starting Training ")
-solver.train_dataset(agent, screen, data_loader, num_epochs=EPOCHS, learning_rate=0.00025)
+solver.train_dataset(agent, screen, data_loader, num_epochs=EPOCHS, learning_rate=2e-7)
+
+logger.info("Creating Benchmark scores")
+best_score, mean_score, mean_dur = solver.play(agent, screen, num_sequences=INITIAL_LEN)
+logger.info("Benchmark with best score %d (Mean %d in %d frames)" % (best_score, mean_score, mean_dur))
 
 logger.info("Saving Model")
-agent.model.save('/models/dqn_base.pt')
-self.logger.info("Done!")
+agent.model.save('output/dqn_base_spaceinvaders.pt')
+logger.info("Done!")
