d003afa237c8fbad6195c1d1f4237342c646981d
ga87six <florian.windolf@tum.de>
2018-02-01 16:59:44 +0100

    Restructured reinforce to work with framework (online only)

--- agents/reinforce.py
+++ agents/reinforce.py
@@ -8,6 +8,7 @@ import torch.nn.functional as F
 import torch.nn.utils as utils
 import torch.optim as optim
 
+from agents.base import AgentBase
 from models.policy import DiscretePolicy, ContinuousPolicy
 
 pi = Variable(torch.FloatTensor([math.pi])).cuda()
@@ -20,36 +21,50 @@ def normal(x, mu, sigma_sq):
     return a*b
 
 
-class REINFORCE:
+class REINFORCE(AgentBase):
     """Agent for REINFORCE policy gradient algorithm."""
 
-    def __init__(
-        self,
-        hidden_size,
-        num_inputs,
-        action_space,
-        gamma,
-        ckpt_freq,
-        continuous
-    ):
-        """Set up agent, build model, set it up."""
-        self.action_space = action_space
+    def __init__(self, screen, history_len, hidden_size, gamma, continuous):
+        """Set up agent, build model, set it up."""  
+        super().__init__(screen)
+        num_inputs = int(history_len * screen.get_dim())
+        num_outputs = screen.get_actions()
+        
         if continuous:
-            self.model = ContinuousPolicy(hidden_size,
-                                          num_inputs,
-                                          action_space)
+            self.model = ContinuousPolicy(hidden_size, num_inputs, num_outputs)
         else:
-            self.model = DiscretePolicy(hidden_size, num_inputs, action_space)
+            self.model = DiscretePolicy(hidden_size, num_inputs, num_outputs)
+            
         self.continuous = continuous
-        self.model = self.model.cuda()
-        self.optimizer = optim.Adam(self.model.parameters(), lr=1e-3)
+        
+        if torch.cuda.is_available:
+            self.model = self.model.cuda()
+            
         self.gamma = gamma
-        self.ckpt_freq = ckpt_freq
 
         # sets model in training mode.
         self.model.train()
-
-    def select_discrete_action(self, state):
+        
+        # for stepping
+        self.done = True
+        self.obs = None
+        self.dur = 0
+        
+    def next_action(self, observation, random=False):
+        if random:
+            action, log_prob, entropy = torch.LongTensor(
+                [self.screen.sample_action()]), 0, 0
+        if self.continuous:
+            action, log_prob, entropy = self.__select_continuous_action(
+                observation)
+        else:
+            action, log_prob, entropy = self.__select_discrete_action(
+                observation)
+            
+        return action.unsqueeze(1), log_prob, entropy
+    
+    
+    def __select_discrete_action(self, state):
         probs = self.model(Variable(state).cuda())
         action = probs.multinomial().data
         prob = probs[:, action[0, 0]].view(1, -1)
@@ -57,7 +72,7 @@ class REINFORCE:
         entropy = - (probs*probs.log()).sum()
         return action[0], log_prob, entropy
 
-    def select_continuous_action(self, state):
+    def __select_continuous_action(self, state):
         mu, sigma_sq = self.model(Variable(state).cuda())
         sigma_sq = F.softplus(sigma_sq)
 
@@ -69,63 +84,122 @@ class REINFORCE:
 
         log_prob = prob.log()
         return action, log_prob, entropy
+        
+    def __encode_frame(self, frame):
+        # if this is an image, make sure it is float 
+        if np.issubdtype(self.obs.dtype, np.integer):
+            self.obs = self.obs.astype(np.float32) / 255.
+        
+        # create FloatTensor
+        if len(self.obs.shape) == 1:
+            state = torch.FloatTensor(self.obs)
+        elif len(self.obs.shape) == 2:
+            state = torch.FloatTensor(self.obs).unsqueeze(0)
+        else:
+            state = torch.FloatTensor(self.obs)       
+            
+        # return with batchsize 0
+        return state.unsqueeze(0)
+        
+    def step(self, screen, save=False, random=False, render=False):
+        """
+        Stepwise playing (with state)
+        
+        Args:
+            screen : wrapper for the environment
+            epoch (int) : current epoch (for epsilon calculation)
+            max_epoch (int) : last epoch (for epsilon calculation)
+            save (bool) : Write to replay memory
+        """
+        # Initialize
+        if self.done is True:            
+            self.obs = screen.reset() # new sequence
+            self.rew = 0 # accumulated
+            self.dur = 0 # accumulated               
+            # for updating params later
+            self.entropies = []
+            self.log_probs = []
+            self.rewards = []
+            
+        # next step in environment - model takes history into account
+        action, log_prob, entropy  = self.next_action(
+            self.__encode_frame(self.obs),random=random)
+        
+        # display screen if render is active
+        if render:
+            screen.render()
+        
+        # get new screen from environment
+        self.next_obs, reward, self.done = screen.input(action[0,0])
+        
+        # store entropies, log_probs
+        if save == True:
+            self.entropies.append(entropy)
+            self.log_probs.append(log_prob)
+            self.rewards.append(reward)
+                        
+        # accumulated reward
+        self.rew += reward
+        self.dur += 1
+        
+        # store negative reward when sequence ends
+        if self.done:
+            reward = -1        
+        
+        self.obs = self.next_obs
+        
+        return self.obs, reward, action, self.done
+    
+    def play(self, screen, max_duration=10000, save=False):
+        """
+        Play a sequence
+        
+        Args:
+            screen : wrapper for the environment
+            epoch (int) : current epoch (for epsilon calculation)
+            max_epoch (int) : last epoch (for epsilon calculation)
+            max_duration (int) : number of frames to be played 
+            save (bool) : Write to replay memory
+        """
+        
+        # while game not lost/terminated
+        done = False        
+        while not done and self.dur < max_duration:            
+            _, _, _, done = self.step(screen, save)
+            
+        return self.rew, self.dur
 
-    def update_parameters(self, rewards, log_probs, entropies, gamma):
+    def optimize(self, optimizer, screen, batchsize, data=None):
         R = torch.zeros(1, 1)
         loss = 0
+        
+        if data is None:
+            # only optimize after a full episode
+            if self.done == False:
+                return 0
+            
+            rewards = self.rewards
+            entropies = self.entropies
+            log_probs = self.log_probs
+            
+        else:
+            # TODO:  generate entropies and log_probs from dataset
+            raise NotImplemented()
+                        
+        # discount rewards
         for i in reversed(range(len(rewards))):
-            R = gamma * R + rewards[i]
-            loss = loss \
-                - (
-                    log_probs[i]*(Variable(R).expand_as(log_probs[i])).cuda()
-                  ).sum() \
-                - (0.0001*entropies[i].cuda()).sum()
+            R = self.gamma * R + rewards[i]
+            loss = loss - (log_probs[i] * (Variable(R).expand_as(
+                log_probs[i])).cuda()).sum() - (0.0001*entropies[i].cuda()).sum()
+        
+        # mean of rewards
         loss = loss / len(rewards)
-        self.optimizer.zero_grad()
+        # backprop
+        optimizer.zero_grad()
         loss.backward()
         utils.clip_grad_norm(self.model.parameters(), 40)
-        self.optimizer.step()
+        optimizer.step()
 
-    def train(self, env, num_episodes, max_episode_length):
-        """Trains the model."""
-        ckpt_dir = 'checkpoints'
-        if not os.path.exists(ckpt_dir):
-            os.mkdir(ckpt_dir)
-        rewards_cum = list()
+        return loss.data.cpu().numpy()
 
-        for i_episode in range(num_episodes):
-            state = torch.Tensor([env.reset()])
-            entropies = []
-            log_probs = []
-            rewards = []
-            for t in range(max_episode_length):
-                if self.continuous:
-                    action, log_prob, entropy = \
-                        self.select_continuous_action(state)
-                else:
-                    action, log_prob, entropy = \
-                        self.select_discrete_action(state)
-                action = action.cpu()
 
-                next_state, reward, done, _ = env.step(action.numpy()[0])
-
-                entropies.append(entropy)
-                log_probs.append(log_prob)
-                rewards.append(reward)
-                state = torch.Tensor([next_state])
-
-                if done:
-                    break
-
-            self.update_parameters(rewards, log_probs, entropies, self.gamma)
-
-            rewards_cum.append(np.sum(rewards))
-            if i_episode % self.ckpt_freq == 0:
-                torch.save(
-                    self.model.state_dict(),
-                    os.path.join(ckpt_dir, 'reinforce-'+str(i_episode)+'.pkl'))
-                print("Episode: {}, reward: {}".format(
-                    i_episode, np.sum(rewards)
-                ))
-
-        return rewards_cum
