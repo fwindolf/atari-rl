0b1f2ac862d28852a409dc9853604167720ba065
ga87six <florian.windolf@tum.de>
2018-02-01 17:00:42 +0100

    Rewrite policy models to work with Reinforce

--- models/policy.py
+++ models/policy.py
@@ -1,35 +1,30 @@
 import torch.nn as nn
 import torch.nn.functional as F
 
+from models.base import ModelBase
 
-class DiscretePolicy(nn.Module):
-    def __init__(self, hidden_size, num_inputs, action_space):
-        super(DiscretePolicy, self).__init__()
-        self.action_space = action_space
-        num_outputs = action_space.n
-
+class DiscretePolicy(ModelBase):
+    def __init__(self, hidden_size, num_inputs, num_outputs):
+        super().__init__(num_inputs, num_outputs)
         self.linear1 = nn.Linear(num_inputs, hidden_size)
         self.linear2 = nn.Linear(hidden_size, num_outputs)
 
-    def forward(self, inputs):
-        x = inputs
+    def forward(self, x):
+        x = x.view(1, -1)
         x = F.relu(self.linear1(x))
         action_scores = self.linear2(x)
-        return F.softmax(action_scores, 1)
+        return F.softmax(action_scores, dim=1)
 
 
-class ContinuousPolicy(nn.Module):
-    def __init__(self, hidden_size, num_inputs, action_space):
-        super(ContinuousPolicy, self).__init__()
-        self.action_space = action_space
-        num_outputs = action_space.shape[0]
-
+class ContinuousPolicy(ModelBase):
+    def __init__(self, hidden_size, num_inputs, num_outputs):
+        super().__init__(num_inputs, num_outputs)
+        
         self.linear1 = nn.Linear(num_inputs, hidden_size)
         self.linear2 = nn.Linear(hidden_size, num_outputs)
         self.linear2_ = nn.Linear(hidden_size, num_outputs)
 
-    def forward(self, inputs):
-        x = inputs
+    def forward(self, x):
         x = F.relu(self.linear1(x))
         mu = self.linear2(x)
         sigma_sq = self.linear2_(x)
