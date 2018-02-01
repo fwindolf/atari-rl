23a1e5b4237970b15689f4222eee2ebffb6d6993
ga87six <florian.windolf@tum.de>
2018-02-01 16:56:13 +0100

    Introduced all mandatory methods

--- agents/base.py
+++ agents/base.py
@@ -13,23 +13,36 @@ class AgentBase:
         self.memory = None
         self.model = None
         
-    def initialize(self):
+    def initialize(self, screen, num_frames=0):
         """
         Initialize the replay buffer and model
         """
+        pass # not necessarily needed
+     
+    def step(self, screen, save=False, random=False, render=False):
+        """
+        Step once in the environment
+        """
         raise NotImplemented()
         
-    def play(self, screen, num_sequences):
+    def play(self, screen, max_duration=10000, save=False):
         """
         Play in the environment to achieve the highest score
         """
-        raise NotImplemented()             
-    
-    def next_action(self, observation):
+        raise NotImplemented()      
+        
+    def optimize(self, optimizer, screen, batchsize, data=None):
         """
-        Select the next action via epsilon-greedy exploration
+        Optimize the model with either data or from stepping
+        """
+        raise NotImplemented()
+    
+    def next_action(self, observation, random=False):
+        """
+        Select the next action
         
         observation (frame) : The current state of the screen        
+        random (bool)       : Should a random action be used
         return (int)        : The best action calculated by the agent
         """        
         raise NotImplemented()
